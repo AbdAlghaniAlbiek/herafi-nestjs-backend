9205ebdee9f77d965502b250459013bcab7c9246
Abdulghani Albaik <abdalghanialbiek@gmail.com>
2023-04-01 23:00:58 +0300

    Complete settings controller with its repo

--- src/apps/admin-desktop/admin-desktop.module.ts
+++ src/apps/admin-desktop/admin-desktop.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './controllers/users.controller';
 import { UsersRepo } from 'src/data/repositories/admin-repos/users.repo';
+import { CraftmenController } from './controllers/craftmen.controller';
+import { CraftmenRepo } from 'src/data/repositories/admin-repos/craftmen.repo';
+import { SettingsController } from './controllers/settings.controller';
+import { SettingsRepo } from 'src/data/repositories/admin-repos/settings.repo';
 
 @Module({
-	controllers: [UsersController],
-	providers: [UsersRepo]
+	controllers: [UsersController, CraftmenController, SettingsController],
+	providers: [UsersRepo, CraftmenRepo, SettingsRepo]
 })
 export class AdminDesktopModule {}
---
+++ src/apps/admin-desktop/controllers/settings.controller.ts
@@ -0,0 +1,41 @@
+import { Get, Param, ParseIntPipe, VERSION_NEUTRAL } from '@nestjs/common';
+import {
+	ApiBadRequestResponse,
+	ApiInternalServerErrorResponse,
+	ApiOkResponse,
+	ApiOperation,
+	ApiParam
+} from '@nestjs/swagger';
+import { AdminProfileDto } from 'src/data/dtos/admin-dtos/responses/settings-respons.dto';
+import { SettingsRepo } from 'src/data/repositories/admin-repos/settings.repo';
+import { ApiController } from 'src/helpers/decorators/swagger.decorator';
+
+@ApiController({ path: 'settings', version: VERSION_NEUTRAL })
+export class SettingsController {
+	constructor(private settingsRepo: SettingsRepo) {}
+
+	//#region
+	@ApiOperation({ summary: 'Get admin profile data' })
+	@ApiOkResponse({
+		description: "Getting admin's profile successfuly",
+		type: AdminProfileDto
+	})
+	@ApiBadRequestResponse({
+		description:
+			'When Param not match specified rules || When id not match any admin account in db'
+	})
+	@ApiInternalServerErrorResponse({
+		description: 'When TypeOrm related error just occurs'
+	})
+	@ApiParam({
+		name: 'adminId',
+		required: true,
+		type: Number,
+		description: 'admin id in db'
+	})
+	//#endregion
+	@Get('admin-profile/:adminId')
+	public getAdminProfile(@Param('adminId', ParseIntPipe) adminId: number) {
+		return this.settingsRepo.getAdminProfile(adminId);
+	}
+}
